a35b214d5fe29cf621a143fc2d1d442cf3089736
EMA <ema@company.local>
2014-12-16 15:10:32 +0100

    GitMain changed to 4

--- GitTest/src/MainClass.java
+++ GitTest/src/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			System.out.println("Hello, World. Main 3");
+			System.out.println("Hello, World. Main 4");
 	}
 
 }
