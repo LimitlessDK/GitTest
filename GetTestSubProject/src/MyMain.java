982e67d095cb8c46f11550ef0faad2611fd83798
EMA <ema@company.local>
2014-12-16 15:11:44 +0100

    Sub changed to 4

--- GetTestSubProject/src/MyMain.java
+++ GetTestSubProject/src/MyMain.java
@@ -3,7 +3,7 @@ public class MyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello,  World. Sub");
+		System.out.println("Hello,  World. Sub 4");
 	}
 
 }
